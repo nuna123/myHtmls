7f8e1ab50b3e666f231fefb088de68911ebca32d
noona <naomindel@gmail.com>
2022-11-26 14:14:20 +0100

    fixed remove func, added remove with linked list

--- linkedListsPractice.c
+++ linkedListsPractice.c
@@ -46,36 +46,6 @@ int populate_list_ptop(t_Node **node_list, char **strs_arr, int arr_size)
 		}
 	return 0;
 }
-/* 
-int populate_list_ptop(t_Node **node_list, char **strs_arr, int arr_size)
-{
-	t_Node *temp_node = NULL;
-	t_Node *list_head = *node_list;
-	for (int i = 0; i < arr_size; i++)
-	{
-		if(!list_head)
-		{
-			list_head = malloc(sizeof(t_Node));
-			*node_list = list_head;
-			(list_head)->str_val = malloc(sizeof(char) * n_strlen(strs_arr[i]) + 1);
-			
-			if(!list_head){return 1;}
-			strcpy((list_head)->str_val,strs_arr[i]);
-			(list_head)->next_node = NULL;
-		}
-		else{
-			temp_node = malloc(sizeof(t_Node));
-			if(!temp_node){return 1;}
-			temp_node->str_val = malloc(sizeof(char) * n_strlen(strs_arr[i]) + 1);
-			strcpy(temp_node->str_val,strs_arr[i]);
-			temp_node->next_node = NULL;
-			while ((list_head)->next_node != NULL)
-				list_head = (list_head)->next_node;
-		}
-		(list_head)->next_node = temp_node;
-		}
-	return 0;
-} */
 
 int	display_list(t_Node *node_list)
 {
@@ -85,6 +55,76 @@ int	display_list(t_Node *node_list)
 		if (node_list->str_val)
 			printf("Index: {%i};\tValue:{%s};\n", index++, node_list->str_val);
 		node_list = node_list->next_node;
+	}
+	return 0;
+}
+
+int remove_item_if_matches(t_Node **node_list, char *poppable_val)
+{
+	if(!node_list){return 1;}
+	t_Node *prev_node = NULL;
+	t_Node *this_node = *node_list;
+	t_Node *node_to_free = NULL;
+
+	while (this_node)
+	{
+		if(strcmp (this_node->str_val, poppable_val) == 0)//if are the same
+		{
+			node_to_free = this_node;
+			//if is the 1st item
+			if (!prev_node){
+				*node_list = this_node->next_node;
+				}
+			else
+			{
+				prev_node->next_node = this_node->next_node;
+			}
+
+			this_node = this_node->next_node;
+
+			free(node_to_free->str_val);
+			free(node_to_free);
+			}
+		else
+		{
+			prev_node = this_node;
+			this_node = this_node->next_node;
+		}
+	}
+	return 0;
+}
+
+int remove_item_linked_func(t_Node **node_list, char *comparable_val, int (cmp_func)())
+{
+	if(!node_list){return 1;}
+	t_Node *prev_node = NULL;
+	t_Node *this_node = *node_list;
+	t_Node *node_to_free = NULL;
+
+	while (this_node)
+	{
+		if((cmp_func)(this_node->str_val, comparable_val) == 0)//if are the same
+		{
+			node_to_free = this_node;
+			//if is the 1st item
+			if (!prev_node){
+				*node_list = this_node->next_node;
+				}
+			else
+			{
+				prev_node->next_node = this_node->next_node;
+			}
+
+			this_node = this_node->next_node;
+
+			free(node_to_free->str_val);
+			free(node_to_free);
+			}
+		else
+		{
+			prev_node = this_node;
+			this_node = this_node->next_node;
+		}
 	}
 	return 0;
 }
@@ -103,10 +143,15 @@ int	free_list(t_Node *node_list)
 int main(void)
 {
 	t_Node *node_list = NULL;
-	char *str_arr[] = {"one", "two", "", "four", "five"};
+	char *str_arr[] = {"one", "two", "three", "four", "five", "one"};
 
-	printf("populate_list_ptop returned:\t%i\n",populate_list_ptop(&node_list, str_arr, (int) sizeof(str_arr) / sizeof(char *)));
+	printf("populmate_list_ptop returned:\t%i\n",populate_list_ptop(&node_list, str_arr, (int) sizeof(str_arr) / sizeof(char *)));
 	printf("display_list returned:\t%i\n", display_list(node_list));
+	printf("remove_item_if_matches returned:\t%i\n", remove_item_if_matches(&node_list, "one"));
+	printf("remove_item_linked_func returned:\t%i\n", remove_item_linked_func(&node_list, "four", strcmp));
+
+	printf("display_list returned:\t%i\n", display_list(node_list));
+
 	printf("free_list returned:\t%i\n", free_list(node_list));
 
 
